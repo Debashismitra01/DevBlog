04e29c6302e8263991eb051ec33d4f978e928927
Debashis Mitra <mdebashis268@gmail.com>
2024-11-15 17:03:56 +0530

    Update route.js

--- src/app/api/blog/route.js
+++ src/app/api/blog/route.js
@@ -28,7 +28,7 @@ export async function GET(request) {
 export async function POST(request) {
     const formData = await request.formData();
     const pass = formData.get('pass');
-    if (pass == process.env.SECRET_PASS){
+    if (pass === process.env.SECRET_PASS){
     const timestamp = Date.now();
 
     const image = formData.get('image');
